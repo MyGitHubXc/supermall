9f1fd54cb87d3e126b106d474b698a2ccc8a8a14
xc <xc>
2021-08-02 22:58:03 +0800

    完成

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,23 @@
 <template>
-<div id="app">
-
-</div>
+  <div id="app" class="wrapper">
+    <keep-alive exclude="Detail">
+      <router-view/>
+    </keep-alive>
+    <main-tab-bar/>
+  </div>
 </template>
 
 <script>
-import MainTabBar from "./components/content/mainTabbar/MainTabBar";
+  import MainTabBar from 'components/content/mainTabbar/MainTabBar'
 
-export default {
-  name: 'App',
-  components: {
+  export default {
+    name: 'app',
+    components: {
+      MainTabBar
+    }
   }
-}
 </script>
 
 <style>
-  @import "./assets/css/base.css";
+  @import "assets/css/base.css";
 </style>
-
